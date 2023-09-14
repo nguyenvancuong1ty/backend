3a44eb7277e2f0a1e2c3a0605e95abba9cf3b39c
Cuong Nguyen 2k1 <nguyenvancuong19032002@gmail.com>
2023-09-14 14:41:58 +0700

    update

--- src/services/CRUDdata.js
+++ src/services/CRUDdata.js
@@ -1,20 +1,20 @@
 const connection = require('../config/connect');
 
 const getUsers = async () => {
-    sql = 'select * from Users';
+    sql = 'select * from users';
     const [result, fields] = await connection.query(sql);
     return result;
 };
 
 const createUser = async (req) => {
     const { email, username, password } = req.body;
-    const sql = `INSERT INTO Users(email, userName,authorization, password) values(?,?,?,?)`;
+    const sql = `INSERT INTO users(email, userName,authorization, password) values(?,?,?,?)`;
     let [result, fields] = await connection.query(sql, [email, username, 0, password]);
 };
 
 const deleteUser = async (req) => {
     const idUser = req.params.id;
-    sql = 'delete from Users where id =?';
+    sql = 'delete from users where id =?';
     const [result, fields] = await connection.query(sql, [idUser]);
 };
 
@@ -22,12 +22,12 @@ const updateUser = async (req) => {
     const { email, name, city } = req.body;
     const idUser = req.params.id;
     console.log(idUser);
-    sql = 'update Users set email = ?, name = ?, city = ? where id = ?';
+    sql = 'update users set email = ?, name = ?, city = ? where id = ?';
     const [result, fields] = await connection.query(sql, [email, name, city, idUser]);
 };
 
 const getUserById = async (req) => {
-    const sql = 'select * from Users where id = ?';
+    const sql = 'select * from users where id = ?';
     const idUser = req.params.id;
     const [result, fields] = await connection.query(sql, [idUser]);
     return result;
