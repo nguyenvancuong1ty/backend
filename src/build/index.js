dc8902fbc11fa922e2bf80e0c4f45093a35539b6
Cuong Nguyen 2k1 <nguyenvancuong19032002@gmail.com>
2023-09-14 17:24:34 +0700

    update v8

--- src/build/index.js
+++ src/build/index.js
@@ -1,21 +1,19 @@
-"use strict";
-
 // import express from 'express';
-var cors = require('cors');
-var express = require('express');
+let cors = require('cors');
+const express = require('express');
 //import scss
-var configViewEngine = require('./config/configViewEngine');
+const configViewEngine = require('./src/config/configViewEngine');
 //import .env get
 require('dotenv').config();
-var webRouter = require('./router/webRouter');
-var apiRouter = require('./router/apiRouter');
-var app = express();
-var port = process.env.PORT;
+const webRouter = require('./src/router/webRouter');
+const apiRouter = require('./src/router/apiRouter');
+
+const app = express();
+const port = process.env.PORT;
+
 app.use(cors());
 app.use(express.json()); // config req.body
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 
 //use
 configViewEngine(app);
@@ -24,12 +22,13 @@ app.use('/v1/api', apiRouter);
 
 //use
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
-  next();
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
+    next();
 });
-app.listen(port, '0.0.0.0', function () {
-  console.log("Express server listening on port ".concat(port));
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Express server listening on port ${port}`);
 });
